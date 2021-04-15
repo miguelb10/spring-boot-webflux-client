fb72772a2561b1e29bac0728532ca4877090b50c
miguelb10 <miguel.blas_03@hotmail.com>
2021-04-15 17:35:56 -0500

    eureka config

--- src/main/java/com/bolsadeideas/springboot/webflux/client/app/models/services/ProductoServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/client/app/models/services/ProductoServiceImpl.java
@@ -22,35 +22,36 @@ import reactor.core.publisher.Mono;
 public class ProductoServiceImpl implements ProductoService {
 
 	@Autowired
-	private WebClient client;
+	private WebClient.Builder client;
 
 	@Override
 	public Flux<Producto> findAll() {
-		return client.get().accept(APPLICATION_JSON).retrieve().bodyToFlux(Producto.class);
+		return client.build().get().accept(APPLICATION_JSON).retrieve().bodyToFlux(Producto.class);
 	}
 
 	@Override
 	public Mono<Producto> findById(String id) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("id", id);
-		return client.get().uri("/{id}", params).accept(APPLICATION_JSON).retrieve().bodyToMono(Producto.class);
+		return client.build().get().uri("/{id}", params).accept(APPLICATION_JSON).retrieve().bodyToMono(Producto.class);
 	}
 
 	@Override
 	public Mono<Producto> save(Producto producto) {
-		return client.post().accept(APPLICATION_JSON).contentType(APPLICATION_JSON).bodyValue(producto).retrieve()
-				.bodyToMono(Producto.class);
+		return client.build().post().accept(APPLICATION_JSON).contentType(APPLICATION_JSON).bodyValue(producto)
+				.retrieve().bodyToMono(Producto.class);
 	}
 
 	@Override
 	public Mono<Producto> update(Producto producto, String id) {
-		return client.put().uri("/{id}", Collections.singletonMap("id", id)).accept(APPLICATION_JSON)
+		return client.build().put().uri("/{id}", Collections.singletonMap("id", id)).accept(APPLICATION_JSON)
 				.contentType(APPLICATION_JSON).bodyValue(producto).retrieve().bodyToMono(Producto.class);
 	}
 
 	@Override
 	public Mono<Void> delete(String id) {
-		return client.delete().uri("/{id}", Collections.singletonMap("id", id)).retrieve().bodyToMono(Void.class);
+		return client.build().delete().uri("/{id}", Collections.singletonMap("id", id)).retrieve()
+				.bodyToMono(Void.class);
 	}
 
 	@Override
@@ -59,8 +60,8 @@ public class ProductoServiceImpl implements ProductoService {
 		parts.asyncPart("file", file.content(), DataBuffer.class).headers(h -> {
 			h.setContentDispositionFormData("file", file.filename());
 		});
-		return client.post().uri("/upload/{id}", Collections.singletonMap("id", id)).contentType(MULTIPART_FORM_DATA)
-				.bodyValue(parts.build()).retrieve().bodyToMono(Producto.class);
+		return client.build().post().uri("/upload/{id}", Collections.singletonMap("id", id))
+				.contentType(MULTIPART_FORM_DATA).bodyValue(parts.build()).retrieve().bodyToMono(Producto.class);
 	}
 
 }
